a96d1ac1f28fc6cf3c4c861f2836e2cbc5dc49dd
Seyz <36236750+Seyz123@users.noreply.github.com>
2019-10-17 00:33:34 +0200

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,7 @@
 const cc = require("../");
 
-setInterval(() => {
-	cc.getOnlineData().then(function(data){
-		console.log(data);
-	}).catch(function(error){
-		console.log(error.stack);
-	});
-}, 500)
+cc.getOnlineData().then(function(data){
+	console.log(data);
+}).catch(function(error){
+	console.log(error.stack);
+});
